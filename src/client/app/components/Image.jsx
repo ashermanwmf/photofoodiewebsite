860645e9f76ec9879d20839808ee8992a1c166aa
ashermanwmf <shermanandrew50@gmail.com>
2017-01-10 21:23:45 -0800

    got google map into modal, next step is adding correct location and resizing for mobile

--- src/client/app/components/Image.jsx
+++ src/client/app/components/Image.jsx
@@ -4,10 +4,38 @@ import { Modal }            from "react-materialize";
 class Image extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mapOptions: {
+        zoom: 14,
+        center: new google.maps.LatLng(37.787507, -122.399838),
+        mapTypeId: "roadmap",
+        mapTypeControl:false,
+        scrollwheel: false,
+        streetViewControl: false,
+        panControl: false,
+        rotateControl: false,
+        zoomControl: true,
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.LEFT_CENTER
+        },
+        styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+      }
+    };
   }
-  openModal(imageData) {
-    //need to set modal and informaiton in redux 
-    console.log(imageData);
+  initMap() {
+    let myLatLng = {lat: 37.787507, lng: -122.399838};
+    let map = new google.maps.Map(this.refs.map, this.state.mapOptions); 
+
+    let marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: "Test"
+    });
+
+    setTimeout(() =>{
+      new google.maps.event.trigger(map, "resize");
+      console.log("init map");
+    }, 500);
   }
   render() {
     let columnClass = window.innerWidth < 750 ? "col s12 photostream-column" : "col s3 photostream-column";
@@ -16,6 +44,7 @@ class Image extends Component {
 
     return(
       <Modal
+        className="modal"
         header={header}
         fixedFooter
         trigger={
@@ -23,7 +52,7 @@ class Image extends Component {
             <img 
               className="photostream-photo" 
               src={this.props.data.images.standard_resolution.url}
-              onClick={() => this.openModal(this.props.data)} 
+              onClick={this.initMap.bind(this)}
             />
           </div>
         }>
@@ -32,10 +61,10 @@ class Image extends Component {
             <img 
                 className="photostream-photo" 
                 src={this.props.data.images.low_resolution.url}
-                onClick={() => this.openModal(this.props.data)} 
             />
           </div>
           <div className={modalColumnClass}>
+            <div className="gMap" ref="map" style={this.state.mapStyling}>I should be a map!</div>
           </div>
         </div>
       </Modal>
